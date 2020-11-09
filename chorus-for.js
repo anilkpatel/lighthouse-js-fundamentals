a948268b3863e6123ff140b80b5f5573f793f46c
anilkpatel <anil.patel@mail.mcgill.ca>
2020-11-09 15:16:33 +0000

    Use a for loop to repeat a chorus line

---
+++ chorus-for.js
@@ -0,0 +1,26 @@
+// MODULE 4: INTRO TO PROGRAMMING: 
+// FOR LOOPS
+
+// Commong structure for looping in JS
+// The idea of a for loop is very similar to a while, only all the code that relates to the state of the loop is written on one line. 
+
+//Anatomy of a for loop:
+//Syntax—a for loop requires 3 statements (separated by semicolons):
+// 1) Initializing the loop (to declare our counter variable
+// 2) Defining our while condition
+// 3) Updating the state of our loop after every step.
+
+// Write a FOR LOOP
+/*
+//Exercise 1: New chorus program
+
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat ++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+*/
+//Same as the While Loop
+//our counter variable, repeat, our condition that should be true for our loop to continue, and the incrementing of our counter variable, are all written in one line.
+
+
